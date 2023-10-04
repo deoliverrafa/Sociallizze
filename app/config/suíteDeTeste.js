5e2b8622fcb96a39070b627eda717f0e629de9e3
deoliverrafa <ytsecretgames38@gmail.com>
2023-10-04 17:10:26 -0300

    InitSuíteDeTestes

---
+++ app/config/suíteDeTeste.js
@@ -0,0 +1,7 @@
+const assert = require('assert');
+
+describe('Suíte de Testes de Usuários', () => {
+    it('Ler um usuário no banco de dados', async () => {
+        console.log("Tested")
+    })
+})
