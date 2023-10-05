8a982b9cd7847ecf7563bb81764389a3a3a6e15a
tarlos40 <carlose.torquato19@gmail.com>
2023-10-05 14:59:58 -0300

    NavBar

--- public/assets/js/nav.js
+++ public/assets/js/nav.js
@@ -1,1 +1,13 @@
-const close = document.querySelectorAll('.icon');
+// VARIAVEIS //
+const icons = document.querySelectorAll('.icon');
+const modals = document.querySelectorAll('.modal');
+
+// CLICK - MENU //
+icons[0].addEventListener('click', () => {
+    modals[0].style.display = 'flex';
+});
+
+// CLICK - X //
+icons[1].addEventListener('click', () => {
+    modals[0].style.display = 'none';
+});
