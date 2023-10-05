b032a997357d7798853eb555e8658daf2370686b
tarlos40 <carlose.torquato19@gmail.com>
2023-10-05 20:38:56 -0300

    Update

--- public/assets/js/nav.js
+++ public/assets/js/nav.js
@@ -1,13 +1,18 @@
-// VARIAVEIS //
-const icons = document.querySelectorAll('.icon');
-const modals = document.querySelectorAll('.modal');
+// IMPORTANDO AS VARIAVEIS //
+import { icons, modals, leftBar } from './variables.js';
 
-// CLICK - MENU //
+// ABRIR O MENU //
 icons[0].addEventListener('click', () => {
     modals[0].style.display = 'flex';
+    modals[0].style.animation = 'opacityModal .3s ease-in-out forwards';
+    leftBar[0].style.animation = 'smoothSideBar .5s ease-in-out forwards'; 
 });
 
-// CLICK - X //
+// FECHAR O MENU //
 icons[1].addEventListener('click', () => {
-    modals[0].style.display = 'none';
+    leftBar[0].style.animation = 'closeSmoothSideBar .5s ease-in-out forwards'; 
+    modals[0].style.animation = 'closeOpacityModal .3s ease-in-out forwards';
+    setTimeout(() => {
+        modals[0].style.display = 'none';
+    }, 300);
 });
---
+++ public/assets/js/variables.js
@@ -0,0 +1,7 @@
+// VARIAVEIS //
+const icons = document.querySelectorAll('.icon');
+const modals = document.querySelectorAll('.modal');
+const leftBar = document.querySelectorAll('.left-bar');
+
+// EXPORTANDO AS VARIAVEIS //
+export { icons, modals, leftBar };
