78ffbd2295a3667b12a8dd440af196a9d4094f66
deoliverrafa <ytsecretgames38@gmail.com>
2023-10-06 09:54:05 -0300

    revisãoNaLógica de UserRoutes

--- app/routes/userRoutes.js
+++ app/routes/userRoutes.js
@@ -19,28 +19,36 @@ router.post('/cadastrar', async (req, res) => {
   }
 });
 
+
+// rota para get de usuários passando o parâmetro email e senha
 router.get('/usuarios', async (req, res) => {
   try {
     await connection.connect();
 
+    // get do email e password da query
     const { email, password } = req.query;
 
+    // condicionais para verificação dos parâmetros passados
     if (!email || !password) {
-      return res.status(400).json({ error: "Email e senha são obrigatórios" })
+      return res.status(500).json({ error: "Email e senha são obrigatórios" });
     }
 
-    const result = await context.read({ email, password})
+    // busca no banco de dados através do context
+    const result = await context.read({ email, password });
 
-    console.log(result)
-    if (result == [] || result == null || result.length == 0) {
-      return res.status(400).json({error: "Email ou senha errados tente novamente"})
+    // verifica se resultado não é null e id existe no resultado para retorna o result
+    if (result !== null && result.id) {
+      res.json(result);
+    } else {
+      // retorna uma mensagem de erro de email ou senha incorretos
+      return res.status(500).json({ error: "Email ou senha incorretos. Tente novamente" });
     }
-
-    res.json(result)
   } catch (error) {
-    res.status(500).json({ error: 'Erro ao ler banco de dados' })
+    // em caso de tudo der errado é repassado essa mensagem de que não conseguiu ler o banco de dados
+    res.status(500).json({ error: 'Erro ao ler banco de dados' });
   }
-})
+});
+
 // Configure outras rotas para read, update e delete aqui...
 
 module.exports = router;
--- public/assets/js/form.js
+++ public/assets/js/form.js
@@ -14,11 +14,11 @@ document.querySelector('.form').addEventListener('submit', function (event) {
     })
         .then(response => response.json())
         .then(data => {
-            console.log('Usuário encontrado no Banco de dados', data)
+            console.log('Usuário encontrado no Banco de dados',data)
             alert('Usuário encontrado no Banco de dados')
         })
         .catch(error => {
             console.log("Usuário não cadastrado, crie uma conta", error)
-            alert('Usuário não cadastrado, crie uma conta', error)
+            alert('Usuário não cadastrado, crie uma conta',error)
         });
 })
