4386c3b1f1fc8f0fe2476529823b844f99833eac
tarlos40 <127431480+tarlos40@users.noreply.github.com>
2023-10-08 16:46:06 -0300

    update public/assets/js/nav.js

--- public/assets/js/nav.js
+++ public/assets/js/nav.js
@@ -3,16 +3,16 @@ import { icons, iconsClose, modals, leftBar } from './variables.js';
 
 // ABRIR O MENU //
 icons[0].addEventListener('click', () => {
-    modals[1].style.display = 'flex';
-    modals[1].style.animation = 'opacityModal .3s ease-in-out forwards';
+    modals[2].style.display = 'flex';
+    modals[2].style.animation = 'opacityModal .3s ease-in-out forwards';
     leftBar[0].style.animation = 'smoothSideBar .5s ease-in-out forwards'; 
 });
 
 // FECHAR O MENU //
-iconsClose[1].addEventListener('click', () => {
+iconsClose[2].addEventListener('click', () => {
     leftBar[0].style.animation = 'closeSmoothSideBar .5s ease-in-out forwards'; 
-    modals[1].style.animation = 'closeOpacityModal .3s ease-in-out forwards';
+    modals[2].style.animation = 'closeOpacityModal .3s ease-in-out forwards';
     setTimeout(() => {
-        modals[1].style.display = 'none';
+        modals[2].style.display = 'none';
     }, 300);
 });
