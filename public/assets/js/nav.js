64c19cc17d0ea0acdbe3fcfc8b8ae6f2111fdf4d
carlose.torquato19@gmail.com <carlose.torquato19@gmail.com>
2023-10-19 15:52:04 -0300

    fixed animation

--- public/assets/js/nav.js
+++ public/assets/js/nav.js
@@ -2,6 +2,7 @@
 import { icons, iconsClose, modals, leftBar, logos } from './variables.js';
 
 const logoAnimate = document.querySelectorAll('.logo-animate');
+let logoCount = 0;
 
 // ABRIR O MENU //
 icons[0].addEventListener('click', () => {
@@ -21,118 +22,75 @@ iconsClose[2].addEventListener('click', () => {
 
 // EFEITO MOSTRA LOGO COMPLETO //
 logos[0].addEventListener('mouseover', () => {
-    setTimeout(() => {
-        logoAnimate[0].style.display = 'flex';
-        logoAnimate[0].style.animation = 'fade 1.5s ease-in-out forwards 0ms';
-    }, 0);
-    setTimeout(() => {
-        logoAnimate[1].style.display = 'flex';
-        logoAnimate[1].style.animation = 'fade 1.5s ease-in-out forwards 25ms';
-    }, 200);
-    setTimeout(() => {
-        logoAnimate[2].style.display = 'flex';
-        logoAnimate[2].style.animation = 'fade 1.5s ease-in-out forwards 50ms';
-    }, 400);
-    setTimeout(() => {
-        logoAnimate[3].style.display = 'flex';
-        logoAnimate[3].style.animation = 'fade 1.5s ease-in-out forwards 75ms';
-    }, 600);
-    setTimeout(() => {
-        logoAnimate[4].style.display = 'flex';
-        logoAnimate[4].style.animation = 'fade 1.5s ease-in-out forwards 100ms';
-    }, 800);
-    setTimeout(() => {
-        logoAnimate[5].style.display = 'flex';
-        logoAnimate[5].style.animation = 'fade 1.5s ease-in-out forwards 125ms';
-    }, 1000);
-    setTimeout(() => {
-        logoAnimate[6].style.display = 'flex';
-        logoAnimate[6].style.animation = 'fade 1.5s ease-in-out forwards 150ms';
-    }, 1200);
-    setTimeout(() => {
-        logoAnimate[7].style.display = 'flex';
-        logoAnimate[7].style.animation = 'fade 1.5s ease-in-out forwards 175ms';
-    }, 1400);
-    setTimeout(() => {
-        logoAnimate[8].style.display = 'flex';
-        logoAnimate[8].style.animation = 'fade 1.5s ease-in-out forwards 200ms';
-    }, 1600);
-    setTimeout(() => {
-        logoAnimate[9].style.display = 'flex';
-        logoAnimate[9].style.animation = 'fade 1.5s ease-in-out forwards 225ms';
-    }, 1800);
-    setTimeout(() => {
-        logoAnimate[10].style.display = 'flex';
-        logoAnimate[10].style.animation = 'fade 1.5s ease-in-out forwards 250ms';
-    }, 2000);
-});
-
-// EFEITO ESCONDER LOGO COMPLETO //
-logos[0].addEventListener('mouseout', () => {
-    setTimeout(() => {
-        logoAnimate[10].style.animation = 'fade 1.5s ease-in-out forwards 0ms';
+    if(logoCount == 0) {
+       setTimeout(() => {
+           logoAnimate[1].style.display = 'flex';
+       }, 200);
+       setTimeout(() => {
+           logoAnimate[2].style.display = 'flex';
+       }, 400);
+       setTimeout(() => {
+           logoAnimate[3].style.display = 'flex';
+       }, 600);
+       setTimeout(() => {
+           logoAnimate[4].style.display = 'flex';
+       }, 800);
+       setTimeout(() => {
+           logoAnimate[5].style.display = 'flex';
+       }, 1000);
+       setTimeout(() => {
+           logoAnimate[6].style.display = 'flex';
+       }, 1200);
+       setTimeout(() => {
+           logoAnimate[7].style.display = 'flex';
+       }, 1400);
+       setTimeout(() => {
+           logoAnimate[8].style.display = 'flex';
+       }, 1600);
+       setTimeout(() => {
+           logoAnimate[9].style.display = 'flex';
+       }, 1800);
+       setTimeout(() => {
+           logoAnimate[10].style.display = 'flex';
+       }, 2000);
+       setTimeout(() => {
+           logoCount++;
+       }, 1000);
+    };
+    // EFEITO ESCONDER LOGO COMPLETO //
+    if(logoCount == 1) {
         setTimeout(() => {
             logoAnimate[10].style.display = 'none';
-        }, 300);
-    }, 0);
-    setTimeout(() => {
-        logoAnimate[9].style.animation = 'fade 1.5s ease-in-out forwards 25ms';
+        }, 0);
         setTimeout(() => {
             logoAnimate[9].style.display = 'none';
-        }, 300);
-    }, 200);
-    setTimeout(() => {
-        logoAnimate[8].style.animation = 'fade 1.5s ease-in-out forwards 50ms';
+        }, 200);
         setTimeout(() => {
             logoAnimate[8].style.display = 'none';
-        }, 300);
-    }, 400);
-    setTimeout(() => {
-        logoAnimate[7].style.animation = 'fade 1.5s ease-in-out forwards 75ms';
+        }, 400);
         setTimeout(() => {
             logoAnimate[7].style.display = 'none';
-        }, 300);
-    }, 600);
-    setTimeout(() => {
-        logoAnimate[6].style.animation = 'fade 1.5s ease-in-out forwards 100ms';
+        }, 600);
         setTimeout(() => {
             logoAnimate[6].style.display = 'none';
-        }, 300);
-    }, 800);
-    setTimeout(() => {
-        logoAnimate[5].style.animation = 'fade 1.5s ease-in-out forwards 125ms';
+        }, 800);
         setTimeout(() => {
             logoAnimate[5].style.display = 'none';
-        }, 300);
-    }, 1000);
-    setTimeout(() => {
-        logoAnimate[4].style.animation = 'fade 1.5s ease-in-out forwards 150ms';
+        }, 1000);
         setTimeout(() => {
             logoAnimate[4].style.display = 'none';
-        }, 300);
-    }, 1200);
-    setTimeout(() => {
-        logoAnimate[3].style.animation = 'fade 1.5s ease-in-out forwards 175ms';
+        }, 1200);
         setTimeout(() => {
             logoAnimate[3].style.display = 'none';
-        }, 300);
-    }, 1400);
-    setTimeout(() => {
-        logoAnimate[2].style.animation = 'fade 1.5s ease-in-out forwards 200ms';
+        }, 1400);
         setTimeout(() => {
             logoAnimate[2].style.display = 'none';
-        }, 300);
-    }, 1600);
-    setTimeout(() => {
-        logoAnimate[1].style.animation = 'fade 1.5s ease-in-out forwards 225ms';
+        }, 1600);
         setTimeout(() => {
             logoAnimate[1].style.display = 'none';
-        }, 300);
-    }, 1800);
-    setTimeout(() => {
-        logoAnimate[0].style.animation = 'fade 1.5s ease-in-out forwards 250ms';
+        }, 1800);
         setTimeout(() => {
-            logoAnimate[0].style.display = 'flex';
-        }, 300);
-    }, 2000);;
+           logoCount--;
+       }, 1000);
+    };
 });
