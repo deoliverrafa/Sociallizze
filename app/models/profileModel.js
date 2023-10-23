0e2b1d12e2b8974f5d3489467e3e2785c0bb1dd2
deoliverrafa <ytsecretgames38@gmail.com>
2023-10-23 09:41:17 -0300

    Init Profile Logic

---
+++ app/models/profileModel.js
@@ -0,0 +1,66 @@
+// IMPORTANDO AS VÁRIAVEIS //
+
+import { itens } from "../../public/assets/js/variables";
+import { getUserData } from "./loginModel";
+
+
+itens[0].addEventListener('click', async () => {
+    console.log("Olá")
+});
+
+iconsClose[5].addEventListener('click', () => {
+
+});
+
+
+buttonLogOut[0].addEventListener('click', async () => {
+    const dadosUser = await loadUserData()
+
+    window.location.href = 'index.html'
+})
+
+
+document.getElementById('avatarForm').addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+    const formData = new FormData();
+    formData.append('avatar', document.getElementById('avatarInput').files[0]);
+
+    // Obtenha o ID do usuário (substitua pelo método real para obtê-lo)
+    const userId = localStorage.getItem('userId');
+
+    await fetch(`http://localhost:3000/api/updateAvatar?userId=${userId}`, {
+        method: 'PUT',
+        body: formData,
+    }).then((response) => {
+        if (!response.ok) {
+            alert('Erro ao atualizar usuário')
+        }
+        return response.json()
+    })
+        .then((data) => {
+            if (data.error) {
+                console.log(data.error)
+            }
+            alert('Foto de perfil atualizada com sucesso', data )
+        })
+        .catch((error) => {
+            alert(error.message);
+            console.log(error.error);
+        })
+});
+
+async function loadUserData() {
+    try {
+        let userData = await getUserData();
+        return userData
+    }
+    catch (error) {
+        console.log("Erro ao carregar dados: ", error);
+    }
+}
+
+async function logOut() {
+    localStorage.setItem('userLoggedIn', 'false')
+    localStorage.setItem('userId', null)
+}
--- public/assets/js/profile.js
+++ public/assets/js/profile.js
@@ -1,11 +1,10 @@
 // IMPORTANDO AS VÁRIAVEIS //
-import { getUserData } from '../../../app/models/loginModel.js';
-import { itens, modals, iconsClose, cards, leftBar, textNick, buttonLogOut, imageProfile } from './variables.js';
+import { itens, modals, iconsClose, cards, leftBar, textNick, buttonLogOut, bio } from './variables.js';
 
 let profileOpen = false;
 let isExpanded = false;
 const charLimit = 35;
-const bioText = bio[0].innerText;
+const biotext = bio[0].innerText;
 
 itens[0].addEventListener('click', async () => {
 
@@ -23,7 +22,6 @@ itens[0].addEventListener('click', async () => {
         }, 300);
     }
 });
-
 
 iconsClose[5].addEventListener('click', () => {
     if (profileOpen) {
@@ -46,52 +44,6 @@ buttonLogOut[0].addEventListener('click', async () => {
 
     window.location.href = 'index.html'
 })
-
-
-document.getElementById('avatarForm').addEventListener('submit', async (e) => {
-    e.preventDefault();
-
-    const formData = new FormData();
-    formData.append('avatar', document.getElementById('avatarInput').files[0]);
-
-    // Obtenha o ID do usuário (substitua pelo método real para obtê-lo)
-    const userId = localStorage.getItem('userId');
-
-    await fetch(`http://localhost:3000/api/updateAvatar?userId=${userId}`, {
-        method: 'PUT',
-        body: formData,
-    }).then((response) => {
-        if (!response.ok) {
-            alert('Erro ao atualizar usuário')
-        }
-        return response.json()
-    })
-        .then((data) => {
-            if (data.error) {
-                console.log(data.error)
-            }
-            alert('Foto de perfil atualizada com sucesso', data )
-        })
-        .catch((error) => {
-            alert(error.message);
-            console.log(error.error);
-        })
-});
-
-async function loadUserData() {
-    try {
-        let userData = await getUserData();
-        return userData
-    }
-    catch (error) {
-        console.log("Erro ao carregar dados: ", error);
-    }
-}
-
-async function logOut() {
-    localStorage.setItem('userLoggedIn', 'false')
-    localStorage.setItem('userId', null)
-}
 
 // ESCONDER BIO - MOSTRAR BIO //
 if (bioText.length > charLimit) {
