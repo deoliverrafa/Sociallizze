e2f56d3498ab298a28a1d1ba2d0bb79b47df4f03
deoliverrafa <ytsecretgames38@gmail.com>
2023-11-16 20:42:39 -0300

    fix bug Scroll on index

--- public/assets/js/nav.js
+++ public/assets/js/nav.js
@@ -4,13 +4,13 @@ import { icons, iconsClose, modals, leftBar, cards } from './variables.js';
 // ABRIR O MENU //
 icons[0].addEventListener('click', () => {
     modals[2].style.display = 'flex';
-    // modals[2].style.animation = 'opacityModal .3s ease-in-out forwards';
-    // leftBar[0].style.animation = 'smoothSideBar .5s ease-in-out forwards';
+    modals[2].style.animation = 'opacityModal .3s ease-in-out forwards';
+    leftBar[0].style.animation = 'smoothSideBar .5s ease-in-out forwards';
     cards[2].style.animation = 'none';
 
     modals[2].addEventListener('click', () => {
-        // leftBar[0].style.animation = 'closeSmoothSideBar .5s ease-in-out forwards';
-        // modals[2].style.animation = 'closeOpacityModal .3s ease-in-out forwards';
+        leftBar[0].style.animation = 'closeSmoothSideBar .5s ease-in-out forwards';
+        modals[2].style.animation = 'closeOpacityModal .3s ease-in-out forwards';
         setTimeout(() => {
             modals[2].style.display = 'none';
         }, 300);
@@ -19,8 +19,8 @@ icons[0].addEventListener('click', () => {
 
 // FECHAR O MENU //
 iconsClose[0].addEventListener('click', () => {
-    // leftBar[0].style.animation = 'closeSmoothSideBar .5s ease-in-out forwards';
-        // modals[2].style.animation = 'closeOpacityModal .3s ease-in-out forwards';
+    leftBar[0].style.animation = 'closeSmoothSideBar .5s ease-in-out forwards';
+        modals[2].style.animation = 'closeOpacityModal .3s ease-in-out forwards';
     setTimeout(() => {
         modals[2].style.display = 'none';
     }, 300);
