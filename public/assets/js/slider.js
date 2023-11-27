745b2317fb8b155427129000d092ef8246ecb49f
carlose.torquato19@gmail.com <carlose.torquato19@gmail.com>
2023-11-27 00:07:40 -0300

    update slider.js

--- public/assets/js/slider.js
+++ public/assets/js/slider.js
@@ -1,5 +1,6 @@
 import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs';
 
+// CARREGA O SLIDE //
 document.addEventListener('DOMContentLoaded', function () {
     const swiper = new Swiper('.swiper', {
         slidesPerView: 1,
@@ -18,4 +19,15 @@ document.addEventListener('DOMContentLoaded', function () {
             el: '.swiper-scrollbar',
         },
     });
+});
+
+// NAVEGAR ENTRE OS SLIDES //
+const nextButton = document.querySelector('.swiper-button-next');
+nextButton.addEventListener('click', function() {
+    swiper.slideNext();
+});
+
+const prevButton = document.querySelector('.swiper-button-prev');
+prevButton.addEventListener('click', function() {
+swiper.slidePrev();
 });
