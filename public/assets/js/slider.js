58a8071e71cfe495e9c3edf448e4c1f7f255034f
carlose.torquato19@gmail.com <carlose.torquato19@gmail.com>
2023-11-27 05:41:29 -0300

    update post

--- public/assets/js/slider.js
+++ public/assets/js/slider.js
@@ -1,31 +1,16 @@
 import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs';
 
 // CARREGA O SLIDE
-document.addEventListener('DOMContentLoaded', function () {
-    const swiper = new Swiper('.swiper', {
-        direction: 'vertical',
-        loop: true,
-        pagination: {
-            el: '.swiper-pagination',
-            clickable: true,
-        },
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-        },
-        scrollbar: {
-            el: '.swiper-scrollbar',
-        },
-    });
-});
-
-// NAVEGAR ENTRE OS SLIDES //
-const nextButton = document.querySelector('.swiper-button-next');
-nextButton.addEventListener('click', function() {
-    swiper.slideNext();
-});
-
-const prevButton = document.querySelector('.swiper-button-prev');
-prevButton.addEventListener('click', function() {
-swiper.slidePrev();
+const swiper = new Swiper('.swiper', {
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
 });
