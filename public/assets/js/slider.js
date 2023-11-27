f62c9ed6da92db68e20e37dbb5b6d0346828e3db
carlose.torquato19@gmail.com <carlose.torquato19@gmail.com>
2023-11-26 23:33:35 -0300

    update

--- public/assets/js/slider.js
+++ public/assets/js/slider.js
@@ -1,6 +1,10 @@
-var mySwiper = new Swiper('.swiper-container', {
+import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs';
+
+const swiper = new Swiper('.swiper-container', {
   slidesPerView: 1,
   spaceBetween: 10,
+  direction: 'vertical',
+  loop: true,
   pagination: {
     el: '.swiper-pagination',
     clickable: true,
